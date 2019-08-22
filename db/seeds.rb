c2a41d32927d4b29c2c808a1d8f8735a79594320
Sebastian Lipiszko <lipsikko@gmail.com>
2019-08-22 18:04:32 +1000

    Shut down save

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.create!(name: "Mojito")
+Cocktail.create!(name: "Walk of Shame")
+Cocktail.create!(name: "Cuba Libre")
+Cocktail.create!(name: "Magarita")
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
