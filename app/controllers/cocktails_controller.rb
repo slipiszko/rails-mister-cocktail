33e44f007820c4301fc66200014405718def2f86
Sebastian Lipiszko <lipsikko@gmail.com>
2019-08-23 16:29:01 +1000

    No css cocktail list

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,12 +14,22 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
     redirect_to cocktails_path
   end
 
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
